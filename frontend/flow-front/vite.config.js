f1e482d050ce7b9f211041837dcdfe00574afae9
Sneh674 <naiksneh6@gmail.com>
2025-01-25 21:20:47 +0530

    vercel ante vite json js update

--- frontend/flow-front/vite.config.js
+++ frontend/flow-front/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Output directory for both Vercel and Render
+  },
+  server: {
+    historyApiFallback: true, // SPA routing during development
+  },
 })
